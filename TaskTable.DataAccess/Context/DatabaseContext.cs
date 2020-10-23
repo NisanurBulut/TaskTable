983b2cffc7e5da148615a0c81e7a67945bf67297
NisanurBulut <nisanurrunasin@gmail.com>
2020-10-23 14:40:25 +0300

    dbcontext sınıfı yazıldı

---
+++ TaskTable.DataAccess/Context/DatabaseContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TaskTable.Entity.Concrete;
+
+namespace TaskTable.DataAccess.Context
+{
+    public class DatabaseContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("");
+            base.OnConfiguring(optionsBuilder);
+        }
+        public DbSet<Kullanici> Kullanicis { get; set; }
+        public DbSet<Calisma> Calismas { get; set; }
+    }
+}
