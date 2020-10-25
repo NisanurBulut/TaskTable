c599f889f8999103fc9637ff2c9c4dc9116402e3
NisanurBulut <nisanurrunasin@gmail.com>
2020-10-25 14:26:16 +0300

    nlogger kurulum yapıldı

--- TaskTable.Service/Controllers/HomeController.cs
+++ TaskTable.Service/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using TaskTable.Service.CustomLogger;
 
 namespace TaskTable.Service.Controllers
 {
@@ -21,6 +22,10 @@ namespace TaskTable.Service.Controllers
         {
             var exceptionHandlerPathFeature =
                HttpContext.Features.Get<IExceptionHandlerPathFeature>();
+            NLogLogger logger = new NLogLogger();
+            logger.LogWithNLog($"Hatanın olduğu yer : {exceptionHandlerPathFeature.Path}\n " +
+                $"Hata mesajı :{exceptionHandlerPathFeature.Error.Message}" +
+                $"StackTrace: {exceptionHandlerPathFeature.Error.StackTrace}");
             ViewBag.Path = exceptionHandlerPathFeature.Path;
             ViewBag.Message = exceptionHandlerPathFeature.Error.Message;
             return View();
---
+++ TaskTable.Service/CustomLogger/NLogLogger.cs
@@ -0,0 +1,17 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TaskTable.Service.CustomLogger
+{
+    public class NLogLogger
+    {
+        public void LogWithNLog(string message)
+        {
+            var logger = LogManager.GetLogger("loggerfile");
+            logger.Log(LogLevel.Error, message);
+        }
+    }
+}
