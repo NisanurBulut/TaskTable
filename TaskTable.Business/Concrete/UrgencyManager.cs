d242cda283ebc2eff5f8a54719dfe0c9d2e4e0f2
NisanurBulut <nisanurrunasin@gmail.com>
2020-10-27 12:45:05 +0300

    urgency servis yazıldı

--- TaskTable.Business/Concrete/UrgencyManager.cs
+++ TaskTable.Business/Concrete/UrgencyManager.cs
@@ -2,10 +2,30 @@
 using System.Collections.Generic;
 using System.Text;
 using TaskTable.Business.Interfaces;
+using TaskTable.Entity.Concrete;
 
 namespace TaskTable.Business.Concrete
 {
     public class UrgencyManager : IUrgencyService
     {
+        public void AddUrgency(UrgencyEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<UrgencyEntity> GetAllUrgency()
+        {
+            throw new NotImplementedException();
+        }
+
+        public UrgencyEntity GetUrgency(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateUrgency(UrgencyEntity entity)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- TaskTable.Business/Interfaces/IUrgencyService.cs
+++ TaskTable.Business/Interfaces/IUrgencyService.cs
@@ -1,10 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using TaskTable.Entity.Concrete;
 
 namespace TaskTable.Business.Interfaces
 {
-    public interface IUrgencyService:IBaseService
+    public interface IUrgencyService
     {
+        public void AddUrgency(UrgencyEntity entity);
+        public void UpdateUrgency(UrgencyEntity entity);
+        public UrgencyEntity GetUrgency(int id);
+        public List<UrgencyEntity> GetAllUrgency();
+
     }
 }
