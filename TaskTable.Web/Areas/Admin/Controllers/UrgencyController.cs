bb79a63c156b9bf91b9fb771d17c96d8580c16fc
NisanurBulut <nisanurrunasin@gmail.com>
2020-10-27 12:42:17 +0300

    area düzenlemesi yapıldı

---
+++ TaskTable.Web/Areas/Admin/Controllers/UrgencyController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TaskTable.Business.Interfaces;
+
+namespace TaskTable.Web.Areas.Member.Controllers
+{
+    [Area("Admin")]
+    public class UrgencyController : Controller
+    {
+        private readonly IUrgencyService _urgencyService;
+        public UrgencyController(IUrgencyService urgencyService)
+        {
+            _urgencyService = urgencyService;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ TaskTable.Web/Areas/Member/Models/UrgencyListViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TaskTable.Web.Areas.Member.Models
+{
+    public class UrgencyListViewModel
+    {
+        public int Id { get; set; }
+        public string Tanim { get; set; }
+    }
+}
