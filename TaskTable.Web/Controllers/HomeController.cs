831912c396c18c09b50f183c91b5eb0da52ea9a5
NisanurBulut <nisanurrunasin@gmail.com>
2020-10-28 17:02:16 +0300

    register backend yazıldı

--- TaskTable.Web/Controllers/HomeController.cs
+++ TaskTable.Web/Controllers/HomeController.cs
@@ -2,18 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using TaskTable.Business.Interfaces;
+using TaskTable.Entity.Concrete;
+using TaskTable.Web.Models;
 
 namespace TaskTable.Web.Controllers
 {
     public class HomeController : Controller
     {
         // dependency Injection
-        ITaskService _taskServ;
-        public HomeController(ITaskService taskService)
+        private readonly ITaskService _taskService;
+        private readonly UserManager<AppUser> _userManager;
+        public HomeController(ITaskService taskService, UserManager<AppUser> userManager)
         {
-            _taskServ = taskService;
+            _taskService = taskService;
+            _userManager = userManager;
         }
         public IActionResult Index()
         {
@@ -21,6 +26,36 @@ namespace TaskTable.Web.Controllers
         }
         public IActionResult Register()
         {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Register(AppUserViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new AppUser
+                {
+                    Name = model.Name,
+                    Surname = model.Surname,
+                    Email = model.Email,
+                    UserName = model.UserName
+                };
+                var result = await _userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    var addRoleResult = await _userManager.AddToRoleAsync(user, "Member");
+                    if (addRoleResult.Succeeded)
+                        return RedirectToAction("Login");
+                    foreach (var err in addRoleResult.Errors)
+                    {
+                        ModelState.AddModelError("", err.Description);
+                    }
+                }
+                foreach (var err in result.Errors)
+                {
+                    ModelState.AddModelError("", err.Description);
+                }
+            }
             return View();
         }
     }
