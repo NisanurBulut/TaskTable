30ae1e8d96f1b1489daacd4b0b0010f139118674
NisanurBulut <nisanurrunasin@gmail.com>
2020-10-28 17:11:34 +0300

    oturum açma ekranı yazıldı

---
+++ TaskTable.Web/Models/AppUserSignInViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TaskTable.Web.Models
+{
+    public class AppUserSignInViewModel
+    {
+        [Required(ErrorMessage = "Kullanıcı adı boş geçilemez")]
+        [Display(Name = "Kullanıcı Adı")]
+        public string UserName { get; set; }
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "Parola boş geçilemez")]
+        [Display(Name = "Parola")]
+        public string Password { get; set; }
+    }
+}
