de2981a807ce362cf192794f254c593ce57ac737
NisanurBulut <nisanurrunasin@gmail.com>
2020-10-29 22:40:19 +0300

    export işlemleri için interface yazıldı

---
+++ TaskTable.Business/Concrete/FileManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TaskTable.Business.Interfaces;
+
+namespace TaskTable.Business.Concrete
+{
+    public class FileManager : IFileService
+    {
+        public byte[] ExportExcel<T>(List<T> entity) where T : class, new()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ExportPdf<T>(List<T> entity) where T : class, new()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TaskTable.Business/Interfaces/IFileService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskTable.Business.Interfaces
+{
+    public interface IFileService
+    {
+        /// <summary>
+        /// Geriye üretilen pdf dosyasının virtual path'ini döner
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        string ExportPdf<T>(List<T> entity) where T : class, new();
+        /// <summary>
+        /// Geriye excel dosyasını byte dizi olarak döner
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        byte[] ExportExcel<T>(List<T> entity) where T : class, new();
+    }
+}
