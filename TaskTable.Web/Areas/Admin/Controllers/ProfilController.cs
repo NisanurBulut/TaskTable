e4775dbbde1d6f3951619b7b3842023d25f56bb8
NisanurBulut <nisanurrunasin@gmail.com>
2020-11-01 21:30:04 +0300

    profil güncelleme işlemi yazıldı

--- TaskTable.Web/Areas/Admin/Controllers/ProfilController.cs
+++ TaskTable.Web/Areas/Admin/Controllers/ProfilController.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ModelBinding.Metadata;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using TaskTable.Entity.Concrete;
 using TaskTable.Web.Areas.Admin.Models;
 
@@ -32,6 +36,39 @@ namespace TaskTable.Web.Areas.Admin.Controllers
             model.Picture = appUser.Picture;
             model.Surname = appUser.Surname;
             return View(model);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(AppUserListViewModel model, IFormFile picture)
+        {
+            if (ModelState.IsValid)
+            {
+                var appUser = await _userManager.Users.FirstOrDefaultAsync(a => a.Id == model.Id);
+                if (picture != null)
+                {
+                    string picExtension = Path.GetExtension(picture.FileName);
+                    string fileName = new Guid() + picExtension;
+                    string filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/img" + fileName);
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                    {
+                        await picture.CopyToAsync(stream);
+                    }
+                    appUser.Picture = fileName;
+                }
+                appUser.Name = model.Name;
+                appUser.Surname = model.Surname;
+                appUser.Email = model.Email;
+                var result = await _userManager.UpdateAsync(appUser);
+                if (result.Succeeded)
+                {
+                    TempData["message"] = "Güncelleme işlemi başarıyla gerçekleşti.";
+                    return RedirectToAction("Index");
+                }
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+            return View();
         }
     }
 }
