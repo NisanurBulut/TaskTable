43939cdf3a57aa87b0be21e9e35d0e92c197e461
NisanurBulut <nisanurrunasin@gmail.com>
2020-11-02 10:41:03 +0300

    sidebar kısmı için viewcomponent yazıldı

---
+++ TaskTable.Web/Areas/Admin/ViewComponents/Wrapper.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TaskTable.Entity.Concrete;
+using TaskTable.Web.Areas.Admin.Models;
+
+namespace TaskTable.Web.Areas.Admin.ViewComponents
+{
+    public class Wrapper : ViewComponent
+    {
+        // view component geriye task dönemez
+        private readonly UserManager<AppUser> _userManager;
+        public Wrapper(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var user = _userManager.FindByNameAsync(User.Identity.Name).Result;
+            AppUserListViewModel model = new AppUserListViewModel();
+            model.Name = user.Name;
+            model.Picture = user.Picture;
+            model.Id = user.Id;
+            model.Surname = user.Surname;
+            model.Email = user.Email;
+            return View(model);
+        }
+    }
+}
