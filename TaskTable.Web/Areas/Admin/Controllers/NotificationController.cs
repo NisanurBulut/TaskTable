cfff426e6e7968990d1ebeb26a243d048707cbf3
NisanurBulut <nisanurrunasin@gmail.com>
2020-11-03 16:13:31 +0300

    bildirim listeleme ve okunma işlemi tanımlandı

---
+++ TaskTable.Web/Areas/Admin/Controllers/NotificationController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using TaskTable.Business.Interfaces;
+using TaskTable.Entity.Concrete;
+using TaskTable.Web.Areas.Admin.Models;
+
+namespace TaskTable.Web.Areas.Admin.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    [Area("Admin")]
+    // area kavramı yalnızca controllera özgüdür
+    // viewcomponentlerde kullanılamaz
+    // her view component'in bir default'ı olmalıdır.
+    public class NotificationController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly INotificationService _notificationService;
+        public NotificationController(INotificationService notificationService,
+            UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+            _notificationService = notificationService;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+            var notificationList = _notificationService.GetUnReadAll(user.Id);
+            var models = new List<NotificationListViewModel>();
+            foreach(var item in notificationList)
+            {
+                var model = new NotificationListViewModel
+                {
+                    Id = item.Id,
+                    Description = item.Description
+                };
+                models.Add(model);
+            }
+            return View(models);
+        }
+        [HttpPost]
+        public IActionResult ReadNotification(int id)
+        {
+            var notification = _notificationService.Get(id);
+            notification.State = true;
+            _notificationService.Update(notification);
+            return RedirectToAction("Index");
+        }
+    }
+}
