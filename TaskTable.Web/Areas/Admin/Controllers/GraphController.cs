079c7e34cc8cf8316cc6ae53d8d0548f3d74436c
NisanurBulut <nisanurrunasin@gmail.com>
2020-11-06 14:09:54 +0300

    google chart eklemesi yapıdlı

---
+++ TaskTable.Web/Areas/Admin/Controllers/GraphController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TaskTable.Web.Areas.Admin.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    [Area("Admin")]
+    public class GraphController : Controller
+    {
+        public IActionResult Index()
+        {
+            TempData["active"] = "graph";
+            return View();
+        }
+    }
+}
